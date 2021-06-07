90774b47956ffaab45d707b27f32f4b2708d0fe6
Kunjie Fan <32661877+kunjiefan@users.noreply.github.com>
2021-06-07 17:36:51 -0400

    add citations

--- code/method_config.py
+++ code/method_config.py
@@ -5,6 +5,7 @@ import sys, os, json
 # 'drug_indep': whether or not summing features of drug1 and drug2 (False: sum)
 method_config_dict = {
     'deepsynergy_preuer_2018':{
+        # Preuer K, Lewis RPI, Hochreiter S, et al. DeepSynergy: Predicting anti-cancer drug synergy with Deep Learning. Bioinformatics 2018; 34:1538–1546
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
@@ -19,6 +20,7 @@ method_config_dict = {
     },
 
     'nn_xia_2018':{
+        # Xia F, Shukla M, Brettin T, et al. Predicting tumor cell line response to drug pairs with deep learning. BMC Bioinformatics 2018; 19:
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
@@ -33,6 +35,7 @@ method_config_dict = {
     },
 
     'nn_kim_2020':{
+        # Kim Y, Zheng S, Tang J, et al. Anti-cancer Drug Synergy Prediction in Understudied Tissues using Transfer Learning. 2020; 1–21
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
@@ -47,6 +50,7 @@ method_config_dict = {
     },
 
     'AuDNNsynergy_zhang_2018':{
+        # Zhang T. Synergistic Drug Combination Prediction by Integrating Multi-omics Data in Deep Learning Models. arXiv 2018; 
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
@@ -61,6 +65,7 @@ method_config_dict = {
     },
 
     'ERT_jeon_2018':{
+        # Jeon M, Kim S, Park S, et al. In silico drug combination discovery for personalized cancer therapy. BMC Syst. Biol. 2018; 12:
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
@@ -75,6 +80,7 @@ method_config_dict = {
     },
 
     'XGBOOST_janizek_2018':{
+        # Janizek JD, Celik S, Lee S-I. Explainable machine learning prediction of synergistic drug combinations for precision cancer medicine. bioRxiv 2018; 331769
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
@@ -89,6 +95,7 @@ method_config_dict = {
     },
 
     'XGBOOST_celebi_2019':{
+        # Celebi R, Bear Don’t Walk O, Movva R, et al. In-silico Prediction of Synergistic Anti-Cancer Drug Combinations Using Multi-omics Data. Sci. Rep. 2019; 9:1–10
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
@@ -103,6 +110,7 @@ method_config_dict = {
     },
 
     'Logit_li_2020':{
+        # Li J, Huo Y, Wu X, et al. Essentiality and transcriptome-enriched pathway scores predict drug-combination synergy. Biology (Basel). 2020; 9:1–18
         'synergy_data': 'NCI_ALMANAC',
         'cell_data': 'NCI_60',
         'cell_list': 'all',        
